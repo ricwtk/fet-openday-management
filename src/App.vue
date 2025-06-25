2bc9cbc78509b687ee9147415c8e56ac437971a7
Richard Wong <ricwtk@gmail.com>
2025-06-25 23:45:57 +0800

    initiate sidebar

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@ import { PanelLeft } from 'lucide-vue-next'
 </script>
 
 <template>
-  <div class="m-2 p-2 w-100% bg-primary-200 rounded-sm">
+  <div class="topbar m-2 p-2 h-15 w-100% bg-primary-200 rounded-sm">
     <Button variant="text"><PanelLeft></PanelLeft></Button>
-    <Select></Select>
+    <!-- <Select></Select> -->
     <!-- <Button>Test</Button> -->
     <!-- <Checkbox binary/> -->
      <!-- <Drawer :visible="true"></Drawer> -->
   </div>
+
+  <div class="sidebar bg-primary-200 rounded-sm p-2 m-2"></div>
     <!-- <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -20,3 +22,20 @@ import { PanelLeft } from 'lucide-vue-next'
   </div>
   <HelloWorld msg="Vite + Vue" /> -->
 </template>
+
+<style lang="css" scoped>
+.topbar {
+  /* height: 4rem; */
+}
+
+.sidebar {
+  position: fixed;
+  width: 20rem;
+  height: calc(100vh - 8rem);
+  z-index: 999;
+  overflow-y: auto;
+  user-select: none;
+  /* top: 6rem; */
+  /* left: 2rem; */
+}
+</style>
