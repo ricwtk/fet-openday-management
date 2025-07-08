fa522e7c95c41ccc56961ac9fcb647811a16e235
Richard Wong <ricwtk@gmail.com>
2025-07-08 16:33:42 +0800

    update locationmap.js

--- src/data/locationmap.js
+++ src/data/locationmap.js
@@ -1,16 +1,56 @@
 export const locationlist = {
   "FET Labs": {
-    "L2": "map-filename",
-    "L1": "map-filename",
-    "G": "map-filename",
-    "B1": "map-filename",
-    "B2": "map-filename"
+    "L2": {
+      "mapfile": "map-filename",
+      "rooms": {
+        
+      }
+    },
+    "L1": {
+      "mapfile": "map-filename",
+      "rooms": {
+        
+      }
+    },
+    "G": {
+      "mapfile": "map-filename",
+      "rooms": {
+        
+      }
+    },
+    "B1": {
+      "mapfile": "map-filename",
+      "rooms": {
+        
+      }
+    },
+    "B2": {
+      "mapfile": "map-filename",
+      "rooms": {
+        
+      }
+    },
   },
   "University Building": {
-    "L3": "map-filename"
+    "L3": {
+      "mapfile": "map-filename",
+      "rooms": {
+        
+      }
+    },
   },
   "Graduate Centre": {
-    "G": "map-filename",
-    "LG": "map-filename",
+    "G": {
+      "mapfile": "map-filename",
+      "rooms": {
+        
+      }
+    },
+    "LG": {
+      "mapfile": "map-filename",
+      "rooms": {
+        
+      }
+    },
   }
 }
