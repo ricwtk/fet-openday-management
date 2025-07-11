0e5d594446e2471dc52efb2d827dcfaf887f6743
Richard Wong <ricwtk@gmail.com>
2025-07-12 00:39:36 +0800

    update form with floatlabel

--- src/components/EventCreation.vue
+++ src/components/EventCreation.vue
@@ -19,17 +19,40 @@ const onSubmit = (event) => {
 <template>
   <Fieldset legend="Create event" :pt="{ 'content': { 'class': 'flex flex-col justify-center' } }">
     <Form v-slot="$form"  @submit="onSubmit" class="flex flex-col gap-2">
-      <InputText name="type" type="text" placeholder="Event Type"></InputText>
-      <InputText name="name" type="text" placeholder="Event Name"></InputText>
-      <CascadeSelect name="venue" ></CascadeSelect>
+      <FloatLabel variant="in">
+        <InputText id="event-type" name="type" type="text" pt:root:class="w-1/1"></InputText>
+        <label for="event-type">Event Type</label>
+      </FloatLabel>
+      <FloatLabel variant="in">
+        <InputText id="event-name" name="name" type="text" pt:root:class="w-1/1"></InputText>
+        <label for="event-name">Event Name</label>
+      </FloatLabel>
+      <FloatLabel variant="in">
+        <CascadeSelect id="event-venue" name="venue" pt:root:class="w-1/1"></CascadeSelect>
+        <label for="event-venue">Venue</label>
+      </FloatLabel>
       <div class="flex gap-2">
-        <DatePicker name="start" placeholder="Start" dateFormat="yy-mm-dd" showTime hourFormat="24" fluid pt:root:class="flex flex-1"></DatePicker>
-        <DatePicker name="end" placeholder="End" fluid pt:root:class="flex flex-1"></DatePicker>
+        <FloatLabel variant="in" class="flex flex-1">
+          <DatePicker id="event-start" name="start" dateFormat="yy-mm-dd" showTime hourFormat="24" fluid pt:root:class="flex flex-1"></DatePicker>
+          <label for="event-start">Start</label>
+        </FloatLabel>
+        <FloatLabel variant="in" class="flex flex-1">
+          <DatePicker id="event-end" name="end" fluid pt:root:class="flex flex-1"></DatePicker>
+          <label for="event-end">End</label>
+        </FloatLabel>
       </div>
-      <InputText name="pic" placeholder="Person In Charge"></InputText>
-      <Textarea name="remarks" placeholder="Remarks"></Textarea>
-      <Button type="submit" severity="secondary" label="Submit" />
+      <FloatLabel variant="in">
+        <InputText id="event-pic" name="pic" pt:root:class="w-1/1"></InputText>
+        <label for="event-pic">Person In Charge</label>
+      </FloatLabel>
+      <FloatLabel variant="in">
+        <Textarea id="event-remarks" name="remarks" pt:root:class="w-1/1"></Textarea>
+        <label for="event-remarks">Remarks</label>
+      </FloatLabel>
+      <Button type="submit" severity="primary" label="Submit" />
     </Form>
   </Fieldset>
+
+  <Message severity="warn">The events are not automatically saved. Copy the JSON object (at the bottom of the page) and update the activities.js for persistent data.</Message>
 
 </template>
