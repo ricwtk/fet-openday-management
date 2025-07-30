3b34390cc099b2fc405ea90d8af37bf51a5be231
Richard Wong <ricwtk@gmail.com>
2025-07-31 00:25:35 +0800

    add ui for listing activities

--- src/components/ByActivities.vue
+++ src/components/ByActivities.vue
@@ -1,12 +1,52 @@
 <script setup>
-import { defineProps } from 'vue';
+import { ref, watch, computed } from 'vue';
+import { LayoutList, List } from 'lucide-vue-next';
+import { useRoute, useRouter } from 'vue-router';
+import { item } from '@primeuix/themes/aura/breadcrumb';
 
 const props = defineProps({
   activities: Array
 })
+
+const route = useRoute();
+const router = useRouter();
+const groupbytype = ref(false)
+if (route.query.groupbytype == "true") {
+  groupbytype.value = true
+}
+watch(groupbytype, (newV) => {
+  router.push({path: route.fullPath, query: { groupbytype: newV }})
+})
+
+const activitytypes = computed(() => {
+  return props.activities.reduce((types, act) => {
+    if (!types.includes(act.type)) { types.push(act.type) }
+    return types
+  }, []).sort()
+})
+
 </script>
 <template>
-  <Panel>
-    {{ props.activities }}
+  <Panel pt:content:class="flex flex-col justify-center">
+    <ToggleButton v-model="groupbytype" onLabel="Grouped by activity type" offLabel="Group by activity type">
+      <template #icon="{value}"><LayoutList v-if="value" /><List v-else /></template>
+    </ToggleButton>
+    <!-- {{ props.activities }}
+    <br>
+    {{ activitytypes }} -->
+    <DataView :value="props.activities" pt:root:class="my-3">
+      <template #list="slotProps">
+        <div v-if="groupbytype" class="flex flex-col gap-2" >
+          <Panel v-for="t in activitytypes" :header="t" toggleable pt:content:class="flex flex-col gap-2">
+            <template v-for="(item, index) in slotProps.items" :key="index">
+              <Activity :activity="item" v-if="item.type == t"></Activity>
+            </template>
+          </Panel>
+        </div>
+        <div v-else class="flex flex-col gap-2">
+          <Activity v-for="(item, index) in slotProps.items" :key="index" :activity="item"></Activity>
+        </div>
+      </template>
+    </DataView>
   </Panel>
 </template>
