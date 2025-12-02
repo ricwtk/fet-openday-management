d2cdf08a1ca1b2931b780987887ad6b3655106b7
Richard Wong <ricwtk@gmail.com>
2025-12-02 11:14:37 +0800

    archive previous event

---
+++ src/data/activities-20250823.js
@@ -0,0 +1,242 @@
+export const activitylist = [
+  {
+    "type": "Showcase",
+    "name": "Robotic Arms, Humanoid and Quadruped Robots, HiWonder AI Mobile Robot",
+    "venue": [
+      "FET Labs",
+      "G",
+      "Collaborative Robots and Industrial Automation (CRIA) Lab"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Dr Riady", "Tan Din Xuen"
+    ],
+    "remarks": ""
+  },
+  {
+    "type": "Showcase",
+    "name": "Sarawak Drone",
+    "venue": [
+      "FET Labs",
+      "B1",
+      "Exhibition Area"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Chew Zhe Zhi"
+    ],
+    "remarks": "Whiteboard x1, Large Monitor x1, Table and chair x1, Barrier pole (Barricades) x6"
+  },
+  {
+    "type": "Showcase",
+    "name": "Robocon",
+    "venue": [
+      "FET Labs",
+      "L2",
+      "The Zone"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Tang Wei Liang",
+      "Wong Jing Sian",
+      "Joe Tan",
+      "Cheryl Phun"
+    ],
+    "remarks": null
+  },
+  {
+    "type": "Showcase",
+    "name": "Oil Palm Harvesting Robot",
+    "venue": [
+      "University Building",
+      "UG",
+      "Opp. Library"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Tang Tiong Yew",
+      "Mohanad Hany Mohamed Abdelhalim Nassar",
+      "Jun Shen Wong",
+      "Lan Yoon Shin",
+      "Ismail Mehtiyev",
+      "Danil Ziyatdinov",
+      "Bassam Shakeel",
+    ],
+    "remarks": "Two tables, One whiteboards"
+  },
+  {
+    "type": "Showcase",
+    "name": "SRC Ottobot",
+    "venue": [
+      "FET Labs",
+      "B1",
+      "Engineering Project Lab"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Fidela",
+      "Oh Kuan Qi",
+      "Ng Yi Zhi",
+      "+1"
+    ],
+    "remarks": "One table"
+  },
+  {
+    "type": "Showcase",
+    "name": "SED Prototype",
+    "venue": [
+      "FET Labs",
+      "B1",
+      "Engineering Project Lab"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Lim Sheng Yung"
+    ],
+    "remarks": ""
+  },
+  {
+    "type": "Showcase",
+    "name": "IDP BEEE Prototypes",
+    "venue": [
+      "FET Labs",
+      "B1",
+      "Engineering Project Lab"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Dr Chia",
+      "Tang Wei Liang",
+      "Lim Zhan Jin"
+    ],
+    "remarks": "Table, Two whiteboards"
+  },
+  {
+    "type": "Workshop",
+    "name": "AI Mobile Robot",
+    "venue": [
+      "FET Labs",
+      "G",
+      "Collaborative Robots and Industrial Automation (CRIA) Lab"
+    ],
+    "timing": [
+      "2025-08-23T11:00:00",
+      "2025-08-23T12:30:00"
+    ],
+    "pic": [
+      "Dr Richard",
+      "Dr Riady"
+    ],
+    "remarks": "HiWonder JetAuto"
+  },
+  {
+    "type": "Workshop",
+    "name": "Building Smarter Interactive Analysis with Power BI",
+    "venue": [
+      "FET Labs",
+      "L1",
+      "Advanced Computing Lab 1"
+    ],
+    "timing": [
+      "2025-08-23T11:00:00",
+      "2025-08-23T12:30:00"
+    ],
+    "pic": [
+      "Dr Samuel"
+    ],
+    "remarks": ""
+  },
+  {
+    "type": "Talk",
+    "name": "Exploring the Future after SPM/IGCSE: Pathways in AI and Psychology",
+    "venue": [
+      "University Building",
+      "L1",
+      "Lecture Theatre 6"
+    ],
+    "timing": [
+      "2025-08-23T14:00:00",
+      "2025-08-23T15:00:00"
+    ],
+    "pic": [
+      "Prof Lin Mei-Hua",
+      "Dr Saad Aslam"
+    ],
+    "remarks": ""
+  },
+  {
+    "type": "Showcase",
+    "name": "FYP (BCHE/BEEE) Posters",
+    "venue": [
+      "University Building",
+      "UG",
+      "iLabs Makerspace"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Prof Ker",
+      "Prof Nishanth",
+      "Ooi Ke Vin"
+    ],
+    "remarks": "two whiteboards"
+  },
+  {
+    "type": "Showcase",
+    "name": "URock",
+    "venue": [
+      "University Building",
+      "UG",
+      "iLabs Makerspace"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Sagili Bharadwaj Reddy",
+      "Chong"
+    ],
+    "remarks": "One table"
+  },
+  {
+    "type": "Showcase",
+    "name": "DP BCHE Prototype",
+    "venue": [
+      "University Building",
+      "UG",
+      "iLabs Makerspace"
+    ],
+    "timing": [
+      "2025-08-23T10:00:00",
+      "2025-08-23T16:00:00"
+    ],
+    "pic": [
+      "Dr Ng"
+    ],
+    "remarks": "one table"
+  }
+]
